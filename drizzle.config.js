b6238de8e1fb00cbf71516bd1e209514433f2754
Rashid Shaikh <rashidddsk32@gmail.com>
2025-04-24 16:13:18 +0530

    Added Vercel Speed Insights & Analytics

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:it2NTy5gGIDH@ep-falling-lab-a8132su9.eastus2.azure.neon.tech/ai-mock-interview?sslmode=require',
+      url: 'postgresql://neondb_owner:it2NTy5gGIDH@ep-falling-lab-a8132su9-pooler.eastus2.azure.neon.tech/ai-mock-interview?sslmode=require',
     }
   };
